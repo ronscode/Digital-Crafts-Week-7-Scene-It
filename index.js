faeed900efc39a0ce573e9e83f684ea971d9bb1c
Ron Cagle <ron@atlantashirt.com>
2018-11-09 13:08:09 -0500

    Part 2 is now working correctly.

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
 // 
- document.addEventListener('DOMContentLoaded', function() {
-
-    function renderMovies(movieData) {
+function renderMovies(movieData) {
     
 
-        var moviesHTML = movieData.map(function(currentMovie){
-             return `
-             <div class="card m-2" style="width: 14rem;">
-             <img id="movieImage"class="card-img-top" src="${currentMovie.Poster}" alt="Movie Image">
-             <div class="card-body">
-               <h5 id="movieTitle" class="card-title">${currentMovie.Title}</h5>
-               <p id="movieYear" class="card-text">${currentMovie.Year}</p>
-               <button class="btn btn-primary" onclick='saveToWatchList("${currentMovie.imdbID}")'>Add Flick</button>
-             </div>
-           </div>
-             `
-         });
-     
-         return moviesHTML.join('');
+    var moviesHTML = movieData.map(function(currentMovie){
+         return `
+         <div class="card m-2" style="width: 14rem;">
+         <img id="movieImage"class="card-img-top" src="${currentMovie.Poster}" alt="Movie Image">
+         <div class="card-body">
+           <h5 id="movieTitle" class="card-title">${currentMovie.Title}</h5>
+           <p id="movieYear" class="card-text">${currentMovie.Year}</p>
+           <button class="btn btn-primary" onclick='saveToWatchList("${currentMovie.imdbID}")'>Add Flick</button>
+         </div>
+       </div>
+         `
+     });
+ 
+     return moviesHTML.join('');
 
-         
-     }
+     
+ }
 
 // This is Scene It pt 2 with the save to watchlist function
 
-    function saveToWatchList(imdbID) {
-        var movie = movieData.find(function(currentMovie) {
-            return currentMovie.imdbID == imdbID;
 
-    });
-    var watchlistJSON = localStorage.getItem(‘watchlist’);
-    var watchlist = JSON.parse(watchlistJSON);
+function saveToWatchList(imdbID) {
+    var movie = movieData.find(function(currentMovie) {
+        return currentMovie.imdbID == imdbID;
 
-    if (!watchlist) {
-        watchlist = [];
-    }
-    watchlist.push(movie);
-    watchlistJSON = JSON.stringify(watchlist);
-    localStorage.setItem('watchlist', watchlistJSON);
+});
 
-    }
 
+var watchlistJSON = localStorage.getItem('watchlist');
+var watchlist = JSON.parse(watchlistJSON);
+
+if (!watchlist) {
+    watchlist = [];
+}
+watchlist.push(movie);
+watchlistJSON = JSON.stringify(watchlist);
+localStorage.setItem('watchlist', watchlistJSON);
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+
+var movieContainer = document.getElementById('movieContainer')
 
 
 document.getElementById('search-form').addEventListener('submit', function(e){
     e.preventDefault();
     var searchMovies = document.getElementById('search-form').value;
-
-
+    
     movieContainer.innerHTML = renderMovies(movieData);
+
 
 });
 
--- watchlist.js
+++ watchlist.js
@@ -1,22 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
+// Setting a variable for the element with the container for the movies. 
 
-    function renderMovies(movieData) {
+var movieContainer = document.getElementById('movieContainer');
+
+function renderMovies(movies) {
     
 
-        var moviesHTML = movieData.map(function(currentMovie){
-             return `
-             <div class="card m-2" style="width: 14rem;">
-             <img id="movieImage"class="card-img-top" src="${currentMovie.Poster}" alt="Movie Image">
-             <div class="card-body">
-               <h5 id="movieTitle" class="card-title">${currentMovie.Title}</h5>
-               <p id="movieYear" class="card-text">${currentMovie.Year}</p>
-               <button class="btn btn-primary" onclick='saveToWatchList("${currentMovie.imdbID}")'>Add Flick</button>
-             </div>
-           </div>
-             `
-         });
-     
-         return moviesHTML.join('');
+  var moviesHTML = movies.map(function(currentMovie){
+       return `
+       <div class="card m-2" style="width: 14rem;">
+       <img id="movieImage"class="card-img-top" src="${currentMovie.Poster}" alt="Movie Image">
+       <div class="card-body">
+         <h5 id="movieTitle" class="card-title">${currentMovie.Title}</h5>
+         <p id="movieYear" class="card-text">${currentMovie.Year}</p>
+         <button class="btn btn-primary" onclick='saveToWatchList("${currentMovie.imdbID}")'>Add Flick</button>
+       </div>
+     </div>
+       `
+   });
 
-         
-     }
+   // Here you are joining the .map and returning it to the inner HTML of the element with the id
+   // movieContainer
+
+   movieContainer.innerHTML = moviesHTML.join('');
+
+   
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    var watchlist = localStorage.getItem('watchlist');
+    var watchlistJSON = JSON.parse(watchlist);
+
+    renderMovies(watchlistJSON);
+
+ 
+
+  
+
+
+});
